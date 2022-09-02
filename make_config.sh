3e8052042c68421a1e089c167cdfa31f18eb7b27
OneDub <60428639+xtrastadia@users.noreply.github.com>
2022-09-02 11:30:02 +1000

    Create make_config.sh

---
+++ make_config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# First argument: Client identifier
+
+KEY_DIR=~/client-config/keys
+OUTPUT_DIR=~/client-config/files
+BASE_CONFIG=~/client-config/base.conf
+
+cat ${BASE_CONFIG} \
+    <(echo -e '<ca>') \
+    ${KEY_DIR}/ca.crt \
+    <(echo -e '</ca>\n<cert>') \
+    ${KEY_DIR}/${1}.crt \
+    <(echo -e '</cert>\n<key>') \
+    ${KEY_DIR}/${1}.key \
+    <(echo -e '</key>\n<tls-crypt>') \
+    ${KEY_DIR}/ta.key \
+    <(echo -e '</tls-crypt>') \
+    > ${OUTPUT_DIR}/${1}.ovpn
