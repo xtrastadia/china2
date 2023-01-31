fb4f2873b601842ff38385ac11eef5e79258912d
OneDub <60428639+xtrastadia@users.noreply.github.com>
2023-01-31 12:01:02 +1000

    Add files via upload

---
+++ test12345.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./test123 -a nexapow -o stratum+tcp://pool.woolypooly.com:3124 -u nexa:nqtsq5g5z64a0mfn4gph0mqw9ew6ls82e35mqgvtygp0p59h -w $HOSTNAME --log-file logs/123.log --no-watchdog --no-tui
